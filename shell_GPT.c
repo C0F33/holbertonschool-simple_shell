6ac4605be0db2c676d069ef29f24f788218e43a9
angel bermudez <angelo1998bg@gmail.com>
2024-04-22 14:32:11 -0400

    shell01

--- shell_GPT.c
+++ shell_GPT.c
@@ -2,28 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <unistd.h> // For chdir, execve
-#include <sys/wait.h> // For wait
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
-#define PATH_MAX 4096 // Maximum length of a file path
+#define PATH_MAX 4096
 
-// Function to handle built-in commands
 void handle_builtin(char *command) {
     if (strcmp(command, "cd") == 0) {
-        // Get the next token which should be the directory
+
         char *directory = strtok(NULL, " ");
         if (directory == NULL) {
             fprintf(stderr, "cd: missing directory\n");
             return;
         }
 
-        // Change directory
+
         if (chdir(directory) != 0) {
             perror("cd");
         }
     } else if (strcmp(command, "exit") == 0) {
-        // Exit the shell
+
         exit(EXIT_SUCCESS);
     }
 }
@@ -44,42 +43,41 @@ int main() {
             printf("\n");
             break;
 		}
-        // Remove trailing newline if present
+
         if (buffer[bytes_read - 1] == '\n') {
             buffer[bytes_read - 1] = '\0';
         }
 
-        // Tokenize the input
+
         token = strtok(buffer, " ");
 
         if (token != NULL) {
-            // Check if it's a built-in command
+
             handle_builtin(token);
 
 
         }
 
-    // Attempt to execute the command from /bin
+
 
 		 char full_path[PATH_MAX];
         sprintf(full_path, "%s", buffer);
 
         if (access(full_path, X_OK) == 0) {
-            // Fork a child process to execute the command
+
             pid_t pid = fork();
 
             if (pid == -1) {
                 perror("fork");
             } else if (pid == 0) {
-                // Child process
+
                 char *args[] = {buffer, NULL};
                 execve(full_path, args, NULL);
-                // If execve fails, print error message
+
                 perror("execve");
                 exit(EXIT_FAILURE);
             } else {
-                // Parent process
-                // Wait for the child process to complete
+
                 wait(NULL);
             }
 		}
@@ -89,7 +87,6 @@ int main() {
 		printf("simple_shell$ ");
 	}
 
-    // Free dynamically allocated buffer
 
 	free(buffer);
 
