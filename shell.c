236e9e89e3c314ed8bf6961a3b301e189d38834c
angel bermudez <angelo1998bg@gmail.com>
2024-04-24 15:29:00 -0400

    oshi

--- shell.c
+++ shell.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <ctype.h>
 #include "shell.h"
 
 int main() {
@@ -17,22 +16,12 @@ int main() {
 
     while ((read = getline(&buffer, &size, stdin)) != -1) {
 
-        char *trimmed_buffer = buffer;
-        while (*trimmed_buffer && isspace(*trimmed_buffer)) {
-            trimmed_buffer++;
-        }
-
-        if (*trimmed_buffer == '\0') {
-
-            continue;
-        }
-
         if (buffer[read - 1] == '\n') {
             buffer[read - 1] = '\0';
         }
 
-        token_handler(trimmed_buffer, " ");
-        process_handler(trimmed_buffer);
+        token_handler(buffer, " ");
+        process_handler(buffer);
 
         if (interactive) {
             printf("simple_shell$ ");
