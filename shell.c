831a58469cf950eca5a69aea394e7864a26b04b5
angel bermudez <angelo1998bg@gmail.com>
2024-04-24 15:23:15 -0400

    fixing trailing white space error

--- shell.c
+++ shell.c
@@ -1,11 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 #include "shell.h"
 
 int main() {
     char *buffer = NULL;
     size_t size = 0;
     ssize_t read;
-
 
     int interactive = isatty(STDIN_FILENO);
 
@@ -14,12 +16,23 @@ int main() {
     }
 
     while ((read = getline(&buffer, &size, stdin)) != -1) {
+
+        char *trimmed_buffer = buffer;
+        while (*trimmed_buffer && isspace(*trimmed_buffer)) {
+            trimmed_buffer++;
+        }
+
+        if (*trimmed_buffer == '\0') {
+
+            continue;
+        }
+
         if (buffer[read - 1] == '\n') {
             buffer[read - 1] = '\0';
         }
 
-        token_handler(buffer, " ");
-        process_handler(buffer);
+        token_handler(trimmed_buffer, " ");
+        process_handler(trimmed_buffer);
 
         if (interactive) {
             printf("simple_shell$ ");
