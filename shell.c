8073b5d70811e9e32755d6338feeab5f5633d840
SergioLebron22 <lebronsergio22@gmail.com>
2024-04-26 09:54:17 -0400

    Added Vonzels email to Authors file

--- shell.c
+++ shell.c
@@ -6,40 +6,41 @@
  * @argv: argv for the command
  * Return: Always returns 0.
  */
+
 int main(int argc, char *argv[])
 {
-    char *user_input = NULL;
-    size_t size_of_buffer = 0;
-    ssize_t stream_status = 0;
-    char *name_of_program = argv[0];
+	char *user_input = NULL;
+	size_t size_of_buffer = 0;
+	ssize_t stream_status = 0;
+	char *name_of_program = argv[0];
 
-    (void)argc;
+	(void)argc;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO) == 1)
-            write(1, "simple_shell$ ", 13);
-        stream_status = getline(&user_input, &size_of_buffer, stdin);
-        if (stream_status == -1)
-        {
-            if (isatty(STDIN_FILENO) == 1)
-                write(1, "\n", 1);
-            break;
-        }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO) == 1)
+			write(1, "simple_shell$ ", 14);
+		stream_status = getline(&user_input, &size_of_buffer, stdin);
+		if (stream_status == -1)
+		{
+			if (isatty(STDIN_FILENO) == 1)
+				write(1, "\n", 1);
+			break;
+		}
 
-        if (user_input[stream_status - 1] == '\n')
-            user_input[stream_status - 1] = '\0';
-        if (*user_input == '\0')
-            continue;
-        if (read_command(user_input, stream_status, name_of_program) == 2)
-        {
-            free(user_input);
-            exit(EXIT_SUCCESS);
-        }
-    }
-    free(user_input);
-    user_input = NULL;
-    return (0);
+		if (user_input[stream_status - 1] == '\n')
+			user_input[stream_status - 1] = '\0';
+		if (*user_input == '\0')
+			continue;
+		if (read_command(user_input, stream_status, name_of_program) == 2)
+		{
+			free(user_input);
+			exit(EXIT_SUCCESS);
+		}
+	}
+	free(user_input);
+	user_input = NULL;
+	return (0);
 }
 
 /**
@@ -49,32 +50,33 @@ int main(int argc, char *argv[])
  * @name_of_program: program name
  * Return: 0
  */
-int read_command(char *input, size_t __attribute__((unused)) stream_status, char *name_of_program)
+int read_command(char *input, size_t __attribute__((unused)) stream_status,
+		char *name_of_program)
 {
-    char *token = NULL;
-    char *cmd_arr[100];
-    int index_i;
+	char *token = NULL;
+	char *cmd_arr[100];
+	int index_i;
 
-    if (input == NULL)
-        return (0);
+	if (input == NULL)
+		return (0);
 
-    if (strcmp(input, "exit") == 0)
-        return (2);
+	if (strcmp(input, "exit") == 0)
+		return (2);
 
-    if (strcmp(input, "env") == 0)
-        print_env();
+	if (strcmp(input, "env") == 0)
+		print_env();
 
-    token = strtok(input, " "), index_i = 0;
-    while (token)
-    {
-        cmd_arr[index_i++] = token;
-        token = strtok(NULL, " ");
-    }
-    if (index_i == 0)
-        return (0);
+	token = strtok(input, " "), index_i = 0;
+	while (token)
+	{
+		cmd_arr[index_i++] = token;
+		token = strtok(NULL, " ");
+	}
+	if (index_i == 0)
+		return (0);
 
-    cmd_arr[index_i] = NULL;
-    return (call_command(cmd_arr, name_of_program));
+	cmd_arr[index_i] = NULL;
+	return (call_command(cmd_arr, name_of_program));
 }
 
 /**
@@ -84,7 +86,7 @@ int read_command(char *input, size_t __attribute__((unused)) stream_status, char
  */
 void print_not_found(char *cmd, char *name_of_program)
 {
-    fprintf(stderr, "%s: %s: command not found\n", name_of_program, cmd);
+	fprintf(stderr, "%s: %s: command not found\n", name_of_program, cmd);
 }
 
 /**
@@ -95,39 +97,39 @@ void print_not_found(char *cmd, char *name_of_program)
  */
 int call_command(char *cmd_arr[], char *name_of_program)
 {
-    char *cmd = cmd_arr[0];
-    struct stat st;
-    char *exe_path_str = NULL;
-    int result;
+	char *cmd = cmd_arr[0];
+	struct stat st;
+	char *exe_path_str = NULL;
+	int result;
 
-    if (cmd == NULL)
-        return (0);
+	if (cmd == NULL)
+		return (0);
 
-    if (cmd[0] == '/' || strncmp(cmd, "./", 2) == 0)
-    {
-        if (stat(cmd, &st) == 0)
-        {
-            result = execute_command(cmd_arr, name_of_program);
-            return (result);
-        }
-        else
-        {
-            print_not_found(cmd, name_of_program);
-            return (3);
-        }
-    }
+	if (cmd[0] == '/' || strncmp(cmd, "./", 2) == 0)
+	{
+		if (stat(cmd, &st) == 0)
+		{
+			result = execute_command(cmd_arr, name_of_program);
+			return (result);
+		}
+		else
+		{
+			print_not_found(cmd, name_of_program);
+			return (3);
+		}
+	}
 
-    exe_path_str = search_path(cmd);
-    if (exe_path_str == NULL)
-    {
-        print_not_found(cmd, name_of_program);
-        return (3);
-    }
+	exe_path_str = search_path(cmd);
+	if (exe_path_str == NULL)
+	{
+		print_not_found(cmd, name_of_program);
+		return (3);
+	}
 
-    cmd_arr[0] = exe_path_str;
-    result = execute_command(cmd_arr, name_of_program);
-    free(exe_path_str);
-    return (result);
+	cmd_arr[0] = exe_path_str;
+	result = execute_command(cmd_arr, name_of_program);
+	free(exe_path_str);
+	return (result);
 }
 
 /**
@@ -138,24 +140,24 @@ int call_command(char *cmd_arr[], char *name_of_program)
  */
 int execute_command(char *cmd_arr[], char *name_of_program)
 {
-    pid_t child_pid;
-    int status;
+	pid_t child_pid;
+	int status;
 
-    child_pid = fork();
-    if (child_pid < 0)
-    {
-        perror("Error:");
-        return (-1);
-    }
-    if (child_pid > 0)
-    {
-        wait(&status);
-    }
-    else if (child_pid == 0)
-    {
-        execve(cmd_arr[0], cmd_arr, environ);
-        perror(name_of_program);
-        exit(1);
-    }
-    return (0);
+	child_pid = fork();
+	if (child_pid < 0)
+	{
+		perror("Error:");
+		return (-1);
+	}
+	if (child_pid > 0)
+	{
+		wait(&status);
+	}
+	else if (child_pid == 0)
+	{
+		execve(cmd_arr[0], cmd_arr, environ);
+		perror(name_of_program);
+		exit(1);
+	}
+	return (0);
 }
